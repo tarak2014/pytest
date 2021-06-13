05e3f3083e036f738420a95f3290c57d7da75bfb
tarak2014 <tarak2014@gmail.com>
2021-06-13 22:49:19 +0530

    pytest

---
+++ basicmath/util.py
@@ -0,0 +1,11 @@
+def add_num(first_no, second_no):
+    return first_no + second_no
+
+def sub_num(first_no, second_no):
+    return first_no - second_no
+
+def mul_num(first_no, second_no):
+    return first_no * second_no
+
+def div_num(first_no, second_no):
+    return first_no / second_no
